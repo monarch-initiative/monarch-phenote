ff31a36863f87677107191c9863f5cae00fa2c7d
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2015-07-21 11:23:35 -0700

    updated minerva files

--- phenote-minerva/bin/start-minerva.sh
+++ phenote-minerva/bin/start-minerva.sh
@@ -3,7 +3,7 @@ set -e
 # Any subsequent commands which fail will cause the shell script to exit immediately
 
 ## Change for different max memory settings 
-MINERVA_MEMORY="512M"
+MINERVA_MEMORY="5g"
 ## Default Minerva Port
 MINERVA_PORT=6800
 
@@ -13,4 +13,5 @@ java "-Xmx$MINERVA_MEMORY" -jar m3-server.jar \
 -c ../../examples/catalog-v001.xml \
 -g ../../examples/minerva-importer.owl \
 -f "$HOME"/owlData \
+--no-reasoning \
 --port $MINERVA_PORT
